2ff10ba0bdba29c972ff031058919e2bad1293bc
omayconmendes <mayconsouzamendes@gmail.com>
2021-11-02 15:14:06 -0300

    add jump function
    add selectors, events and functions;
    add function 'jump' to script.js;

--- script.js
+++ script.js
@@ -13,4 +13,31 @@ function handleKeyup(event) {
         console.log('Pressionou espaço!')
     }
 
+    jump();
+
+}
+
+
+function jump() {
+    
+    let position = 0;
+
+    let upInterval = setInterval( () => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval( () => {
+                if(position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            } , 20);
+        } else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    } , 20);
+    
 }
